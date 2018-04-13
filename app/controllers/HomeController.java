4d96dd63f471f9c7c01885dc45bdf0e89312a7d9
anuraagm <muktevianuraag@gmail.com>
2018-04-13 10:24:47 +0000

    Initial commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -15,7 +15,8 @@ import views.html.login;
 
 import javax.inject.Inject;
 import java.util.Objects;
-
 public class HomeController extends Controller {
-
+    public Result chat(){
+        return ok("Hello World");
+    }
 }
