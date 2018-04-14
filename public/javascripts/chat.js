1a90969259c386a5a7462d74342541ffc47713b4
anuraagm <muktevianuraag@gmail.com>
2018-04-14 12:17:38 +0000

    message page improvements 14/4/2018 17:55

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello Bot!'
+    	},
+        {
+            'sender': 'BOT',
+            'text': 'Hello user! How can I help you?'
+    	},
+        {
+            'sender': 'USER',
+            'text': 'Give me some news about Donald Trump'
+    	},
+        {
+            'sender': 'BOT',
+            'text': 'Here is news about Donald Trump'
+    	},
+        {
+            'sender': 'USER',
+            'text': 'Thank you'
+    	}
+	];
 });
